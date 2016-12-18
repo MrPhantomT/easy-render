8578b80d7189dbc012ab5278affa1e54f098475c
Dementev Roman <infud@yandex.ru>
2016-12-18 23:58:45 +0300

    remove unnecessary files

--- src/easy_render/JGraphic.clj
+++
@@ -1,7 +0,0 @@
-(ns easy-render.JGraphic
-  (:import [java.awt Color])
-  (:gen-class
-    :name "easy-render.JGraphic"
-    :extends java.awt.Graphics
-    :constructors { [clojure.lang.IFn] [] }))
-
